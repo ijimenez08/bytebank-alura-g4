2f619e63ac8905123ba826401f048eb0915a82bf
Isaac <isaac.jimenez@utp.ac.pa>
2023-02-21 21:58:44 -0500

    vc 	modified:   .project 	modified:   bin/Gerente.class 	modified:   src/Gerente.java

--- src/Gerente.java
+++ src/Gerente.java
@@ -1,5 +1,38 @@
+import javax.print.DocPrintJob;
 
 public class Gerente 
 {
+    private String nombre;
+    private String dococumento;
+    private double salario;
+    
+    public void setDococumento(String dococumento) 
+    {
+        this.dococumento = dococumento;
+    }
+    
+    public void setNombre(String nombre) 
+    {
+        this.nombre = nombre;
+    }
+    
+    public void setSalario(double salario) 
+    {
+        this.salario = salario;
+    }
+
+    public String getDococumento() 
+    {
+        return dococumento;
+    }
+
+    public String getNombre() 
+    {
+        return nombre;
+    }
+    public double getSalario() 
+    {
+        return salario;
+    }
 
 }
